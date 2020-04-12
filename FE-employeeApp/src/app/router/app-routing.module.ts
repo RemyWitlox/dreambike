3ff1b0031d8beeafa716ef736c963f82a2325397
unknown <jmgwitlox@hotmail.com>
2020-04-12 14:39:17 +0200

    AB#41 sidenav for Menu inserted

--- FE-employeeApp/src/app/router/app-routing.module.ts
+++ FE-employeeApp/src/app/router/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from '../components/content/content.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/content', pathMatch: 'full' },
+  { path: 'content', component: ContentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
