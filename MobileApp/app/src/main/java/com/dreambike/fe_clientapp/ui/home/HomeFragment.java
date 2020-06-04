10b247d3dcd415743548625b81f81db218d8c0d2
unknown <jmgwitlox@hotmail.com>
2020-06-04 15:57:26 +0200

    AB#28 finetuning

--- MobileApp/app/src/main/java/com/dreambike/fe_clientapp/ui/home/HomeFragment.java
+++ MobileApp/app/src/main/java/com/dreambike/fe_clientapp/ui/home/HomeFragment.java
@@ -7,10 +7,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.dreambike.fe_clientapp.R;
+import com.dreambike.fe_clientapp.services.DockingStation;
 import com.dreambike.fe_clientapp.services.HttpGetRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,8 +29,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class HomeFragment extends Fragment implements OnMapReadyCallback{
@@ -34,6 +38,8 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
   private MapView mMapView;
   private View mView;
   private String token;
+  private ArrayList<DockingStation> dockings;
+
 
   public HomeFragment (){
     // Required empty public constructor
@@ -86,56 +92,40 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         .appendQueryParameter("access_token",token)
         .build().toString();
 
-      String result;
+      String result = "";
       HttpGetRequest getRequest = new HttpGetRequest(token);
 
       result = getRequest.execute(uri).get();
 
-
-      if (result.isEmpty()){
+      if (result == null){
         Toast.makeText(getContext().getApplicationContext(), "Database unavailable", Toast.LENGTH_LONG).show();
       } else {
         // Write the access token of the result:
         Toast.makeText(getContext().getApplicationContext(), "Dockers loaded from backend", Toast.LENGTH_LONG).show();
-        Log.d("result docking stations", result.toString());
+        Log.d("result docking stations", result);
         JSONArray array = new JSONArray(result);
+        dockings = new ArrayList<DockingStation>(array.length());
+
         for (int i = 0; i < array.length(); i++) {
           JSONObject jsonObject = array.getJSONObject(i);
-          String name = jsonObject.getString("name");
-          String lat = jsonObject.getString("lat");
-          String lng = jsonObject.getString("lng");
-          Boolean active = jsonObject.getBoolean("active");
-          String bikes = jsonObject.getString("bikes");
-          String capacity = jsonObject.getString("capacity");
+          DockingStation dock = new DockingStation(
+            jsonObject.getString("name"), jsonObject.getDouble("lng"),jsonObject.getDouble("lat"),jsonObject.getInt("capacity"),jsonObject.getInt("bikes"),jsonObject.getBoolean("active")
+          );
+          dockings.add(dock);
+          int rentable = dock.getCapacity() - dock.getBikes();
 
-
-          HashMap<String, String> ds = new HashMap<>();
-
-          ds.put("name", name);
-          ds.put("lat", lat);
-          ds.put("lng", lng);
-          ds.put("active", active.toString());
-          ds.put("bikes", bikes);
-          ds.put("capacity", capacity);
-
-          double lat1 = Double.parseDouble(lat);
-          double lng1 = Double.parseDouble(lng);
-          int bikes1 = Integer.parseInt(bikes);
-          int capacity1 = Integer.parseInt(capacity);
-          int rentable = capacity1 - bikes1;
-
-          if (active) {
-            googleMap.addMarker(new MarkerOptions().position(new LatLng(lat1, lng1)).title(name).snippet(rentable + " bikes to rent."));
+          if (dock.getActive()) {
+            googleMap.addMarker(new MarkerOptions().position(new LatLng(dock.getLat(), dock.getLng())).title(dock.getName()).snippet(rentable + " bikes to rent."));
           }
+
         }
       }
     } catch (ExecutionException | InterruptedException | JSONException e) {
-      e.printStackTrace();
+      Toast.makeText(getContext().getApplicationContext(), "Database unavailable", Toast.LENGTH_LONG).show();
     }
 
     // Set a position
-    CameraPosition Liberty = CameraPosition.builder().target(home).zoom(16).bearing(0).tilt(45).build();
-
+    CameraPosition Liberty = CameraPosition.builder().target(home).zoom(14).bearing(0).tilt(45).build();
     // Set the camera to the position above
     googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
   }
