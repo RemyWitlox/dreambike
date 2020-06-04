a23745e2f8faae4577d9631ce21caf0822e2f0b5
unknown <jmgwitlox@hotmail.com>
2020-06-04 13:57:32 +0200

    AB#71 fromNewBackend

--- FE-employeeApp/src/app/services/custom-http-interceptor.service.ts
+++ FE-employeeApp/src/app/services/custom-http-interceptor.service.ts
@@ -16,17 +16,17 @@ export class CustomHttpInterceptorService implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    if (localStorage.getItem('access_token')) {
-      let at: string = localStorage.getItem('access_token');
-      let bearer: string = 'Bearer ' + at;
-      let myheaders = new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: bearer,
-        access_token: at,
-      });
-      return next.handle(req.clone({ headers: myheaders }));
-    } else {
-      return next.handle(req.clone());
-    }
+    // if (localStorage.getItem('access_token')) {
+    //   let at: string = localStorage.getItem('access_token');
+    //   let bearer: string = 'Bearer ' + at;
+    //   let myheaders = new HttpHeaders({
+    //     'Content-Type': 'application/json',
+    //     Authorization: bearer,
+    //     access_token: at,
+    //   });
+    //   return next.handle(req.clone({ headers: myheaders }));
+    // } else {
+    return next.handle(req.clone());
+    // }
   }
 }
--- FE-employeeApp/src/app/services/docking.service.ts
+++ FE-employeeApp/src/app/services/docking.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DockingStation } from '../models';
 
@@ -7,11 +7,22 @@ import { DockingStation } from '../models';
   providedIn: 'root',
 })
 export class DockingService {
-  url: string = 'http://localhost:2020/api/getAllDocking';
+  url: string = 'http://localhost:8181/docking/';
 
   constructor(private http: HttpClient) {}
 
   getDockingStations(): Observable<DockingStation[]> {
-    return this.http.get<DockingStation[]>(this.url);
+    const url = this.url + 'getAll';
+    return this.http.get<DockingStation[]>(url);
   }
+
+  createDockingStation(ds: DockingStation): Observable<DockingStation> {
+    const url = this.url + 'newDocking';
+    return this.http.post<DockingStation>(url, ds);
+  }
+
+  // deleteDockingStation(ds: DockingStation): Observable<any> {
+  //   const url = this.url + "deleteDocking";
+  //   return this.http.delete<any>(url, {dockingId: ds.dockingId});
+  // }
 }
